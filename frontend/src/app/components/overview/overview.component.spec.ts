4f4c218c38a062b8d1840e38c919f8449cd5b0e6
Manuel Birrer <manuel@birrer.ch>
2024-02-26 11:42:30 +0100

    overview tests

--- frontend/src/app/components/overview/overview.component.spec.ts
+++ frontend/src/app/components/overview/overview.component.spec.ts
@@ -4,6 +4,7 @@ import {OverviewComponent} from './overview.component';
 import {ActivatedRoute} from "@angular/router";
 import {MealService} from "../../services/meal.service";
 import {of} from "rxjs";
+import {DayOfMeals, Week} from "../../calendar";
 
 describe('OverviewComponent', () => {
   let component: OverviewComponent;
@@ -36,5 +37,48 @@ describe('OverviewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize with meals of current week', () => {
+    const mockDays: DayOfMeals[] = [
+      {
+        date: new Date("2024-01-01"),
+        meals: [
+          {
+            date: new Date("2024-01-01T18:00:00"),
+            recipe: "something",
+            cook: "testCookId",
+          }
+        ]
+      }
+    ];
+    mealService.getMealsOf.and.returnValue(of(mockDays));
+    component.ngOnInit();
+    expect(mealService.getMealsOf).toHaveBeenCalled();
+    expect(component.days).toBe(mockDays);
+  });
+
+  it('should be today', () => {
+    const today = new Date("2024-01-01");
+    component.today = new Date("2024-01-01T18:00:00");
+    expect(component.isToday(today)).toBe(true);
+  });
+
+  it('should not be today', () => {
+    const notToday = new Date("1999-01-01");
+    component.today = new Date("2024-01-01");
+    expect(component.isToday(notToday)).toBe(false);
+  });
+
+  it('should sanitize valid date string', () => {
+    const dateString = "2024-01-01";
+    const sanitizedDate = component.sanitizeDate(dateString);
+    expect(sanitizedDate).toEqual(new Date(dateString));
+  });
+
+  it('should sanitize invalid date string', () => {
+    const dateString = 'invalid-date-string';
+    const sanitizedDate = component.sanitizeDate(dateString);
+    expect(isNaN(sanitizedDate.valueOf())).toBe(false);
   });
 });
